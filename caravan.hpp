20cac8a242c031eac205d1bbe34dadc729fe4af6
Yohsuke Murase <yohsuke.murase@gmail.com>
2021-02-23 14:13:22 +0900

    initial implementation of main

--- caravan.hpp
+++ caravan.hpp
@@ -2,6 +2,7 @@
 #define CARAVAN_HPP
 
 #include <iostream>
+#include <queue>
 #include <functional>
 #include <mpi.h>
 #include <nlohmann/json.hpp>
@@ -9,12 +10,20 @@
 namespace caravan {
   using json = nlohmann::json;
   class Queue {
+    public:
+    int64_t Push(const json& input) {};
   };
   void Start( std::function<void(Queue&)> on_init,
-              std::function<void(Queue&,const json&)> on_result_receive,
+              std::function<void(int64_t,const json&,Queue&)> on_result_receive,
               std::function<json(const json&)> do_task,
               MPI_Comm comm = MPI_COMM_WORLD,
-              int num_proc_per_buf = 384);
+              int num_proc_per_buf = 384) {
+    Queue q;
+    on_init(q);
+    json input = { {"message","hello"} };
+    json output = do_task(input);
+    on_result_receive(0, output, q);
+  }
 }
 
 #endif // CARAVAN_HPP
--- test_caravan.cpp
+++ test_caravan.cpp
@@ -4,9 +4,32 @@
 #include "caravan.hpp"
 
 
+using json = nlohmann::json;
+
 int main(int argc, char* argv[]) {
 
   MPI_Init(&argc, &argv);
+
+  auto on_init = [](caravan::Queue& q) {
+    // pre-process: create json_object that contains parameters of Tasks
+    json input = { {"message","hello"} };
+    uint64_t task_id = q.Push(input);
+
+    std::cerr << "task: " << task_id << " has been created.\n";
+  };
+
+  auto on_result_receive = [](int64_t task_id, const json& output, caravan::Queue& q) {
+    std::cerr << "task: " << task_id << " has finished : " << output << "\n";
+  };
+
+  std::function<json(const json& input)> do_task = [](const json& input) {
+    // do some job
+    json output;
+    output["result"] = input["message"].get<std::string>() + " world";
+    return output;
+  };
+
+  caravan::Start(on_init, on_result_receive, do_task, MPI_COMM_WORLD);
 
   MPI_Finalize();
 
